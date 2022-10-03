18ff7b39b8524feee04303242de9628ec87bd047
Evgen <zhenya.bill@gmail.com>
2022-10-03 11:02:22 +0300

    start

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -11,13 +11,12 @@ class AppController {
 
                 fib[0] = 0;
                 fib[1] = 1;
-                if (index === 0) return res.status(400).json({message: 'index must start with 1'});
+                if (index === 0) { res.status(400).json({ message: 'index must start with 1' }); return }
                 if (index === 1) return fib[0];
                 if (index === 2 || index === 3) return fib[1];
 
                 for (i = 2, b = 1; b <= index; i+=1, b+=1) {
                     fib[i] = fib[i-2] + fib[i-1]; // generate the rest of the fibonacci array
-                    console.log('tick');
                     if (index === b) { // if we find the number
                         return fib[i-2]; //return it
                     }
